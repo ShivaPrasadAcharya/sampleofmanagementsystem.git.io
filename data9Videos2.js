85cf54b2c31a29847b3d92622b3bd1417527ae70
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-09-16 08:31:37 +0000

    okdone2

--- data9Videos2.js
+++ data9Videos2.js
@@ -1,7 +1,7 @@
 // data9video2.js - Videos Dataset for dropdownset
 var data9Videos2 = `ID,Title,Description,shorturl,longurl
 001,Motivation for Exam,Premanadaji,see video,https://www.youtube.com/shorts/Jr4_hHwVfJw
-002,Ninda2,Premanadaji,see video,Videos/aboutnida.mp4
+002,Ninda2,Premanadaji,see video,Videos/aboutninda.mp4
     name: "Videos2",
     description: "Video Gallery2",
     emoji: "🎥",
