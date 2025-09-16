7d1105afe8c0bbd20a06e8f2f7abd333d4375d98
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-09-16 14:44:57 +0545

    Update data9Videos2.js

--- data9Videos2.js
+++ data9Videos2.js
@@ -1,11 +1,18 @@
 // data9video2.js - Videos Dataset for dropdownset
 var data9Videos2 = `ID,Title,Description,shorturl,longurl
-001,Motivation for Exam,Premanadaji,see video,https://www.youtube.com/shorts/Jr4_hHwVfJw
-002,Ninda2,Premanadaji,see video,Videos/aboutninda.mp4
+001,Motivation for Exam4,Premanadaji,see video,https://www.youtube.com/shorts/Jr4_hHwVfJw
+002,about Ninda,Premanandaji,see system video,Videos/aboutninda.mp4
+`;
+
+// Dataset metadata
+var data9Videos2Info = {
     name: "Videos2",
     description: "Video Gallery2",
     emoji: "🎥",
     columns: 5,
     primaryKey: "ID"
 };
+
+
+
 
